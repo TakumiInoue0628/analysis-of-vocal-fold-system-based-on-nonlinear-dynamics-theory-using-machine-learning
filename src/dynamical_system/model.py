40897a1ac466326d28184fef8117a48235b835cb
TakumiInoue <takumi_inoue@outlook.com>
2024-10-20 22:51:40 +0900

    commit

--- src/dynamical_system/model.py
+++ src/dynamical_system/model.py
@@ -17,3 +17,23 @@ def rossler(t, X, parameter_list):
     dX[1] = X[0] + a * X[1]
     dX[2] = b + X[2] * (X[0] - c)
     return dX
+
+### Langford
+def langford(t, X, parameter_list):
+    dX = np.zeros(X.shape)
+    [a, b, c, d, e, f] = parameter_list
+    #dX[0] = a * X[0] + b * X[1] + X[0] * X[2]
+    #dX[1] = c * X[0] + d * X[1] + X[1] * X[2]
+    #dX[2] = e * X[2] - (X[0] * X[0] + X[1] * X[1] + X[2] * X[2])
+    dX[0] = (X[2] - b) * X[0] - d * X[1]
+    dX[1] = d * X[0] + (X[2] - b) * X[1]
+    dX[2] = c + a * X[2] - X[2] * X[2] * X[2] / 3 - (X[0] * X[0] + X[1] * X[1]) * (1 + e * X[2]) + f * X[2] * X[0] * X[0] * X[0]
+    return dX
+
+def torus(t, X, parameter_list):
+    dX = np.zeros(X.shape)
+    [a] = parameter_list
+    dX[0] = X[1]
+    dX[1] = -X[0] - X[1] * X[2]
+    dX[2] = X[1] * X[1] - a
+    return dX
--- src/visualization/figure.py
+++ src/visualization/figure.py
@@ -11,6 +11,135 @@ sys.path.append(parent_dir)
 from src.util import short_time_ft, fft
 from src.util import Normalization
 
+class report20241001():
+
+    def __init__(self, rcParams_dict, savefig=False, save_dir=None, name=None, file_type='png'):
+        for key in rcParams_dict.keys():
+            plt.rcParams[str(key)] = rcParams_dict[str(key)]
+        self.save = savefig
+        self.dir = save_dir
+        self.name = name
+        self.ext = file_type
+
+    def attractor(self, t, X,  
+                plt_num = 5000,
+                f_lim = (0, 1.5),
+                p_lim = (None, None),
+                marker=False,
+                   file_name=''):    
+        
+        ### Attractor
+        spec = gridspec.GridSpec(ncols=1, nrows=1, )
+        fig = plt.figure(figsize=(2.5, 2.5))
+        ax = fig.add_subplot(spec[0], projection='3d')
+        ax.set_title('Attractor', loc='center', fontsize=plt.rcParams['font.size'], pad=-5)
+        if marker: ax.plot(X[:, 0], X[:, 1], X[:, 2], '.', ms=5, lw=0.5, c='k')
+        else: ax.plot(X[:, 0], X[:, 1], X[:, 2], lw=0.5, c='k')
+        ax.set_xlabel(r'$x$', labelpad=-15)
+        ax.set_ylabel(r'$y$', labelpad=-15)
+        ax.set_zlabel(r'$z$', labelpad=-15)
+        ax.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
+        plt.tight_layout()
+        if self.save: 
+            plt.savefig(self.dir+self.name+'_attractor_'+file_name+'_1.'+self.ext, bbox_inches="tight")  
+        plt.show()
+
+        ### Timeseries & Power spectta
+        spec = gridspec.GridSpec(ncols=2, nrows=1, width_ratios=[2.5, 2], wspace=0.4)
+        fig = plt.figure(figsize=(7, 2))
+        ax = fig.add_subplot(spec[0])
+        ax.set_title('Timeseries', fontsize=plt.rcParams['font.size'])
+        ax.plot(t[:plt_num], X[:plt_num, 0], lw=1, c='k')
+        ax.set_xlabel(r'Time [s]')
+        ax.set_ylabel(r'$x(t)$')
+        ax.set_xlim(t[:plt_num][0], t[:plt_num][-1])
+        ax.get_xaxis().set_major_formatter(plt.FormatStrFormatter('%d'))
+        ax = fig.add_subplot(spec[1])
+        ax.set_title('Power spectra', fontsize=plt.rcParams['font.size'])
+        freq_org, amp_org = fft(X[:, 0], t)
+        ax.plot(freq_org, amp_org, lw=1.0, c='k')
+        ax.set_xlabel('Frequency [Hz]')
+        ax.set_ylabel(r'Power')
+        ax.get_yaxis().set_major_formatter(plt.FormatStrFormatter('%.1f'))
+        ax.set_xlim(f_lim[0], f_lim[1])
+        ax.set_ylim(p_lim[0], p_lim[1])
+        if self.save: 
+            plt.savefig(self.dir+self.name+'_attractor_'+file_name+'_2.'+self.ext, bbox_inches="tight")  
+        plt.show()
+
+    def bifurcation(self, parameter, x,  
+                    idx_list,
+                    parameter_arange, 
+                    t_list, X_list, x_list,
+                    select_parameter_list,
+                plt_num = 2000,
+                f_lim = (0.05, 0.2),
+                p_lim = (None, 6.5),
+                   file_name=''):    
+        
+        ### Bifurcation diagram
+        spec = gridspec.GridSpec(ncols=2, nrows=1, height_ratios=[1], hspace=0)
+        fig = plt.figure(figsize=(10, 3))
+        ax = fig.add_subplot(spec[0:2])
+        ax.set_title('Bifurcation diagram', loc='center', fontsize=plt.rcParams['font.size'])
+        ax.scatter(parameter, x, s=0.1, c='b')
+        ax.set_xlabel(r'$b$')
+        ax.set_ylabel(r'$y$')
+        ax.get_xaxis().set_major_formatter(plt.FormatStrFormatter('%.2f'))
+        ax.get_yaxis().set_major_formatter(plt.FormatStrFormatter('%d'))
+        ax.set_xlim(parameter[0], parameter[-1])
+        for p in select_parameter_list: ax.axvline(x=p, linestyle='--', c='r', lw=1.5)
+        if self.save: 
+            plt.savefig(self.dir+self.name+'_bifurcation_'+file_name+'_0.'+self.ext, bbox_inches="tight")  
+        plt.show()
+
+        i = 0
+        for p in select_parameter_list:
+            i += 1
+            p_idx = np.argmin(np.abs(np.array(parameter_arange) - p))
+
+            ### Attractor
+            spec = gridspec.GridSpec(ncols=1, nrows=1, )
+            fig = plt.figure(figsize=(2.5, 2.5))
+            ax = fig.add_subplot(spec[0], projection='3d')
+            ax.set_title('Attractor', loc='center', fontsize=plt.rcParams['font.size'], pad=-5)
+            ax.plot(X_list[p_idx][:, 0], X_list[p_idx][:, 1], X_list[p_idx][:, 2], lw=0.1, c='k')
+            ax.scatter(X_list[p_idx][:, 0][idx_list[p_idx]], x_list[p_idx][idx_list[p_idx]], X_list[p_idx][:, 2][idx_list[p_idx]], s=1, c='b')
+            ax.set_xlabel(r'$x$', labelpad=-15)
+            ax.set_ylabel(r'$y$', labelpad=-15)
+            ax.set_zlabel(r'$z$', labelpad=-15)
+            ax.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
+            ax.view_init(elev=40, azim=170, roll=0)
+            if self.save: 
+                plt.savefig(self.dir+self.name+'_bifurcation_'+file_name+str(i)+'_1.'+self.ext, bbox_inches="tight")  
+            plt.show()
+            
+            ### Timeseries & Power spectra
+            spec = gridspec.GridSpec(ncols=2, nrows=1, width_ratios=[2.5, 2], wspace=0.4)
+            fig = plt.figure(figsize=(7, 2))
+            ax = fig.add_subplot(spec[0])
+            ax.set_title('Timeseries and the local-maxima', fontsize=plt.rcParams['font.size'])
+            ax.plot(t_list[p_idx], x_list[p_idx], lw=1, c='k')
+            ax.scatter(t_list[p_idx][idx_list[p_idx]], x_list[p_idx][idx_list[p_idx]], s=10, c='b')
+            ax.set_xlabel(r'Time [s]')
+            ax.set_ylabel(r'$y(t)$')
+            ax.set_xlim(t_list[p_idx][0], t_list[p_idx][plt_num])
+            ax.get_xaxis().set_major_formatter(plt.FormatStrFormatter('%d'))
+            ax = fig.add_subplot(spec[1])
+            ax.set_title('Power spectra', fontsize=plt.rcParams['font.size'])
+            freq_org, amp_org = fft(x_list[p_idx], t_list[p_idx])
+            ax.plot(freq_org, amp_org, lw=1.0, c='k')
+            ax.set_xlabel('Frequency [Hz]')
+            ax.set_ylabel(r'Power')
+            ax.get_yaxis().set_major_formatter(plt.FormatStrFormatter('%.1f'))
+            ax.set_xlim(f_lim[0], f_lim[1])
+            ax.set_ylim(p_lim[0], p_lim[1])
+            if self.save: 
+                plt.savefig(self.dir+self.name+'_bifurcation_'+file_name+str(i)+'_2.'+self.ext, bbox_inches="tight")  
+            plt.show()
+
+
+
 class signal():
 
     def __init__(self, rcParams_dict, savefig=False, save_dir=None, name=None, file_type='png'):
